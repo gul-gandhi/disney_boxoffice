d66473abef45109092a9a7e63decfa0c3a369d7f
gul-gandhi <57415425+gul-gandhi@users.noreply.github.com>
2020-04-17 17:33:06 +0530

    Update disney.py

--- disney.py
+++ disney.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+#storing information 
 data = pd.read_csv('disney_movies_total_gross.csv', parse_dates=['release_date'])
 print(data.head())
 
@@ -9,13 +11,17 @@ print(datad.head(10))
 
 #extracting year
 data['release_year'] = pd.DatetimeIndex(data['release_date']).year
-#mean per genre per year
+#calculating mean gross earned per genre per year
 group = data.groupby(['genre','release_year']).mean()
+#dataframe
 genre_yearly = group.reset_index()
 print(genre_yearly.head(10))
 
 #sns.relplot(kind='line', x='release_year', y='inflation_adjusted_gross', hue='genre', data=genre_yearly)
-
+#dummy variables for genre
 genre_dummies =  pd.get_dummies(data['genre'], drop_first=True)
 print(genre_dummies.head())
+
+#creating model
+regr = LinearRegression()
 
